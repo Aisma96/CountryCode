29095cfca4533a30bcc5426cf3166e5391859b8d
Aisma96 <amainonyte10gmail.com>
2023-12-14 19:34:43 +0200

    changed population compare code

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 class Country:
-    def __init__(self, population, area, country_name, is_big = True):
+    def __init__(self, population, area, country_name):
         self.population = population
         self.area = area
         self.country_name = country_name
-        self.is_big = is_big
+        self.is_big = self.check_is_big()
+        self.population_density = self.population / self.area
 
-    def pop_grater(self):
-        if self.population > 20000000 or self.area > 3000000:
-            print(True)
-        else:
-            print(False)
+    def check_is_big(self):
+        return self.population > 20000000 or self.area > 3000000
 
-    def compare_pop(self):
-        austr = australia.population / australia.area
-        andor = andorra.population / andorra.area
-        if andor > austr:
-            print(f'{self.country_name} has a larger population density than Australia')
+    def compare_pop(self, other_country):
+        if self.population_density > other_country.population_density:
+            return f'{self.country_name} has bigger density than {other_country.country_name}'
+        elif self.population_density < other_country.population_density:
+            return f'{self.country_name} has less density tahn {other_country.country_name}'
         else:
-            print(f'{andorra} has a smaller population density than {australia}')
+            return f'{self.country_name} has same density as {other_country.country_name}'
 
 
 australia = Country(23545500, 7692024, "Australia")
 andorra = Country(76098, 468, "Andorra")
+print(australia.is_big)
+print(andorra.is_big)
+print(australia.population_density)
+print(andorra.population_density)
+print(andorra.compare_pop(australia))
+print(australia.compare_pop(andorra))
